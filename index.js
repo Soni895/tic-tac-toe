90c34f7742558e3d8a4deb87c5d61736308ed5cb
soni895 <darshansoni895@gmail.com>
2023-06-13 17:31:34 +0530

    code update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let grid_box=document.querySelectorAll(".box");
 let newgame=document.querySelector(".newgame");
 let current_player="X";
 let game_grid;
+
+
+current_player.
 win_possition=[
     [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
 ];
@@ -16,23 +19,31 @@ function init_game()
 }
 init_game()
 
+// grid_box.forEach((value,index)=>
+// {
+//     value.addEventListener("click",()=> display_value(index)
+//      );
+
+// });
+
+
+
 for(let i=0;i<grid_box.length;i++)
 {
-
+    grid_box[i].addEventListener("click",()=>display_value(i)
+    );
 }
+
 function display_value(i)
 {
-
-    console.log("hi darshan");
     if(game_grid[i]==="")
     {
         grid_box[i].textContent=current_player;
         current_player="O";
         game_grid[i]=current_player;
-
     }
 
     else{
-
+return;
     }
 }
